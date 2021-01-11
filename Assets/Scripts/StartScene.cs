67891e9d89ed764d5433653dc26167c65d7250a8
nminhtai <nminhtai@gmail.com>
2021-01-11 17:25:04 +0700

    tracking download percent

--- Assets/Scripts/StartScene.cs
+++ Assets/Scripts/StartScene.cs
@@ -4,51 +4,11 @@ using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.AddressableAssets.ResourceLocators;
 using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.UI;
 
 public class StartScene : MonoBehaviour {
-    //public AssetReference reference;
-    //public Canvas canvas;
-    //void Awake() {
-    //    reference.InstantiateAsync(canvas.transform, false);
-    //}
-
-
-    //void Start() {
-    //    //Caching.ClearCache();
-    //    //Addressables.ClearResourceLocators();
-
-    //    //Debug.Log($"Application.dataPath {Application.dataPath}");
-    //    //Debug.Log($"Application.streamingAssetsPath {Application.streamingAssetsPath}");
-    //    //Debug.Log($"Application.persistentDataPath {Application.persistentDataPath}");
-    //    //Debug.Log($"Application.temporaryCachePath {Application.temporaryCachePath}");
-    //    //Debug.Log($"Caching.currentCacheForWriting.path {Caching.currentCacheForWriting.path}");
-    //    //Debug.Log($"Addressables.BuildPath {Addressables.BuildPath}");
-
-
-    //    //Debug.Log($"UnityEngine.AddressableAssets.Addressables.RuntimePath: {UnityEngine.AddressableAssets.Addressables.RuntimePath}");
-    //    //Debug.Log($"UnityEngine.AddressableAssets.Addressables.BuildPath: {UnityEngine.AddressableAssets.Addressables.BuildPath}");
-    //    //Debug.Log($"UnityEngine.AddressableAssets.Addressables.PlayerBuildDataPath: {UnityEngine.AddressableAssets.Addressables.PlayerBuildDataPath}");
-
-
-    //    //Addressables.LoadSceneAsync("GirlScene");
-    //    Addressables.UpdateCatalogs().Completed += StartScene_Completed; 
-
-
-
-    //}
-
-    //void test() {
-    //    Addressables.InitializeAsync().Completed += objects =>
-    //    {
-    //        Addressables.CheckForCatalogUpdates().Completed += checkforupdates =>
-    //        {
-    //            if (checkforupdates.Result.Count > 0)
-    //                Addressables.UpdateCatalogs().Completed += updates => Load(label);
-    //            else
-    //                Load(LabelsToDownload);
-    //        };
-    //    };
-    //}
+    [SerializeField] Text sizeText;
+    [SerializeField] Text percentText;
 
     const string label = "remote";
     void Start() {
@@ -64,12 +24,15 @@ public class StartScene : MonoBehaviour {
                     Addressables.UpdateCatalogs().Completed += updateHandle => {
                         Debug.Log("UpdateCatalogs complete");
                         Addressables.GetDownloadSizeAsync(label).Completed += sizeOp => {
+                            sizeText.text = sizeOp.Result.ToString();
                             Debug.Log($"GetDownloadSizeAsync_Completed: size {sizeOp.Result}");
                             var handle = Addressables.DownloadDependenciesAsync(label);
                             handle.Completed += downloadHandle => {
                                 Debug.Log("download complete");
+                                percentText.text = "100%";
                                 Addressables.LoadSceneAsync("GirlScene");
                             };
+                            StartCoroutine(PercentTracking(handle));
                         };
                     };
                 }
@@ -81,91 +44,12 @@ public class StartScene : MonoBehaviour {
         };
     }
 
-    IEnumerator PercentTracking() {
-        //Debug.Log("PercentTracking");
-        //Caching.ClearCache();
-        yield return new WaitForSeconds(5);
-
-        Addressables.GetDownloadSizeAsync(label).Completed += op => {
-            Debug.Log($"GetDownloadSizeAsync_Completed: size {op.Result}");
-            var handle = Addressables.DownloadDependenciesAsync(label);
-            while (!handle.IsDone) {
-                Debug.Log($"PercentTracking {handle.PercentComplete}");
-            }
-        };
-    }
-
-    //IEnumerator UpdateCatalogs() {
-    //    List<string> catalogsToUpdate = new List<string>();
-    //    AsyncOperationHandle<List<string>> checkForUpdateHandle = Addressables.CheckForCatalogUpdates();
-    //    checkForUpdateHandle.Completed += op => {
-    //        Debug.Log("UpdateCatalogs completed");
-    //        foreach (var item in catalogsToUpdate) {
-    //            Debug.Log(item);
-    //        }
-    //        catalogsToUpdate.AddRange(op.Result);
-    //    };
-    //    yield return checkForUpdateHandle;
-    //    if (catalogsToUpdate.Count > 0) {
-    //        AsyncOperationHandle<List<IResourceLocator>> updateHandle = Addressables.UpdateCatalogs(catalogsToUpdate);
-    //        yield return updateHandle;
-    //    }
-    //}
-
-
-
-    //private void UpdateCatalogs_Completed(AsyncOperationHandle<List<UnityEngine.AddressableAssets.ResourceLocators.IResourceLocator>> obj) {
-    //    Debug.Log("UpdateCatalogs_Completed");
-    //    if (obj.Result != null) {
-
-    //        foreach (var item in obj.Result) {
-    //            Debug.Log(item.LocatorId);
-    //            foreach (var name in item.Keys) {
-    //                Debug.Log(name);
-
-    //            }
-    //            Addressables.DownloadDependenciesAsync(item).Completed += DownloadDependenciesAsync_Completed;
-    //        }
-    //    }
-
-
-
-    //}
-
-    //private void DownloadDependenciesAsync_Completed(AsyncOperationHandle obj) {
-    //    Debug.Log("DownloadDependenciesAsync_Completed");
-    //    Debug.Log($"{obj},{obj.Result}");
-    //}
-
-    //private void CheckForCatalogUpdates_Completed(AsyncOperationHandle<List<string>> obj) {
-    //    Debug.Log("CheckForCatalogUpdates_Completed");
-    //    if (obj.Result != null) {
-    //        foreach (var item in obj.Result) {
-    //            Debug.Log(item);
-    //        }
-    //    }
-
-    //}
-
-    //private void StartScene_Completed(AsyncOperationHandle<List<UnityEngine.AddressableAssets.ResourceLocators.IResourceLocator>> obj) {
-    //    Addressables.LoadSceneAsync("GirlScene");
-    //}
-
-    //public IEnumerator Start() {
-    //    string key = "GirlScene";
-    //    //Clear all cached AssetBundles
-    //    Addressables.ClearDependencyCacheAsync(key);
-
-    //    //Check the download size
-    //    AsyncOperationHandle<long> getDownloadSize = Addressables.GetDownloadSizeAsync(key);
-    //    yield return getDownloadSize;
-
-    //    //If the download size is greater than 0, download all the dependencies.
-    //    if (getDownloadSize.Result > 0) {
-    //        AsyncOperationHandle downloadDependencies = Addressables.DownloadDependenciesAsync(key);
-    //        yield return downloadDependencies;
-    //    }
-
-    //    Addressables.LoadSceneAsync("GirlScene");
-    //}
+    IEnumerator PercentTracking(AsyncOperationHandle handle) {
+        while (!handle.IsDone) {
+            //Debug.Log($"PercentTracking {handle.PercentComplete}");
+            percentText.text = $"{handle.PercentComplete}";
+            yield return null;
+        }
+        yield break;
+    }    
 }
